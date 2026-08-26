0338188cbf124f62eb2307265ae4a72d0df7ef4a
SUNGWON JO <sungwon000604@gmail.com>
2026-08-26 11:46:59 +0900

    data_loader 필요 없는 함수 삭제

--- data_loader.py
+++ data_loader.py
@@ -9,15 +9,3 @@ class DataLoader:
     def load_csv(self, file_path):
         self.df = pd.read_csv(file_path)
         return self.df
-
-    # dataFrame 행,열 확인
-    def get_shape(self):
-        return self.df.shape
-
-    # 모든 컬럼(열) 리스트로 반환
-    def get_columns(self):
-        return self.df.columns.tolist()
-
-    # 각 컬럼의 데이터 타입 Pandas Series 로 반환
-    def get_dtypes(self):
-        return self.df.dtypes
